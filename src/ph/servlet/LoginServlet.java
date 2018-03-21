cf49305d575f406ab6359f97a53b97cd5f496aa1
hlzhangwhvcse <25538330@qq.com>
2018-03-21 08:59:23 +0800

    add LoginServlet.java by lzhang 20180321 @ 502

---
+++ src/ph/servlet/LoginServlet.java
@@ -0,0 +1,14 @@
+package ph.servlet;
+
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet(name = "LoginServlet")
+public class LoginServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
